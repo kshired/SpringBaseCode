1262e43add50aaf478821c861e8992b8cca65966
kshired <shiroed1211@naver.com>
2022-01-22 23:15:23 +0900

    [INIT] init project

---
+++ src/main/java/com/kshired/springbase/SpringBaseApplication.java
@@ -0,0 +1,12 @@
+package com.kshired.springbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBaseApplication.class, args);
+    }
+}
