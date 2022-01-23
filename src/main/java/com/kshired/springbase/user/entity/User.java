343c8b2175caa4f88863cd730d95be8e6f70214b
kshired <shiroed1211@naver.com>
2022-01-23 19:47:15 +0900

    [MOD] User Entity가 UserDetails를 impl하도록 수정

--- src/main/java/com/kshired/springbase/user/entity/User.java
+++ src/main/java/com/kshired/springbase/user/entity/User.java
@@ -1,29 +1,79 @@
 package com.kshired.springbase.user.entity;
 
 import com.kshired.springbase.user.dto.UserResponseDto;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @NoArgsConstructor
-public class User {
+@Getter
+public class User implements UserDetails {
     @Id
     @GeneratedValue
     private Long id;
 
+    @Column(length = 100, nullable = false, unique = true)
     private String email;
 
+    @Column(length = 100, nullable = false)
     private String password;
 
-    public User(String email, String password) {
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
+
+    public User(String email, String password, List<String> roles) {
         this.email = email;
         this.password = password;
+        this.roles = roles;
     }
 
     public UserResponseDto toUserResponseDto() {
         return new UserResponseDto(this.id, this.email);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
